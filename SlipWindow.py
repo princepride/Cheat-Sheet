53c0a64e32712989c379b71c6b4e6550d2b8d7ca
汪志鹏 <13476859295@163.com>
2022-12-17 23:46:45 +0800

    Create SlipWindow.py

---
+++ SlipWindow.py
@@ -0,0 +1,15 @@
+#给一个字符串s，替换其字串中字符最多k次使其字符都相同，求字串的最大长度
+def character_replacement(self, s: str, k: int) -> int:
+        num = [0] * 26
+        n = len(s)
+        maxn = left = right = 0
+
+        while right < n:
+            num[ord(s[right]) - ord("A")] += 1
+            maxn = max(maxn, num[ord(s[right]) - ord("A")])
+            if right - left + 1 - maxn > k:
+                num[ord(s[left]) - ord("A")] -= 1
+                left += 1
+            right += 1
+        
+        return right - left
